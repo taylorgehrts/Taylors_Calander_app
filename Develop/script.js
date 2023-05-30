6d0bb0c0de09681fdfb3846d690acf5576910e4b
taylorgehrts <taylorgehrts@gmail.com>
2023-05-30 15:25:56 -0700

    updated styling, implemented day.js to display date in header

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,13 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
+var currentDay = dayjs().format("dddd");
+var currentDate = dayjs().format(" MMM, D, YYYY");
+
+
+$("#currentDay").text(currentDay + currentDate);
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
